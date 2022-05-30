b88cc350185f23ae3e042543cb39e005ce30b8e1
Masoud <masoudtabatabaei20@gmail.com>
2022-05-30 23:14:40 +0430

    Featur: Add github link to about component

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -4,6 +4,11 @@ const About = () => {
   return (
     <>
       <Link to="/">Go Back</Link>
+      <div style={{ margin: "12px 0" }}>
+        <a href="https://github.com/masoudtabatabaei" target="_blank">
+          My Github
+        </a>
+      </div>
       <p>Copyright &copy;{new Date().getFullYear()}</p>
     </>
   );
