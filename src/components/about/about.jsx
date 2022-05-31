d77e7a102e75fc6ad916c325dbac79eba0164d2c
Masoud <masoudtabatabaei20@gmail.com>
2022-05-31 23:40:39 +0430

    Fix: Change style of github link

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,11 +1,18 @@
 import { Link } from "react-router-dom";
 
 const About = () => {
+  const githubImgStyle = {
+    verticalAlign: "middle",
+    maxWidth: "30px",
+    marginRight: "10px",
+  };
+
   return (
     <>
       <Link to="/">Go Back</Link>
       <div style={{ margin: "12px 0" }}>
         <a href="https://github.com/masoudtabatabaei" target="_blank">
+          <img style={githubImgStyle} src="/github.png" />
           My Github
         </a>
       </div>
